a08ccbd05a0a973f45b7fbdd8b4f630a8817aff8
RyanDelap <akayungeagle@gmail.com>
2017-05-05 08:56:17 -0500

    Fix bic bug

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import pygame
 from time import sleep
 import sys
 from pygame.locals import *
+import tkFileDialog as filedialog
+import Tkinter as tk
+
 # import docx
 
 #set constant variables
@@ -16,125 +19,141 @@ color = (255, 255, 255)
 #set variables
 i = 0
 
+root = tk.Tk()
+root.withdraw()
+path = filedialog.askopenfilename()
+
 #open the file we want to read
-with open("meinkampf.txt", "r") as meinKampf:
-    textDoc = meinKampf.read()
+with open(path, "r") as textFile:
+    textDoc = textFile.read()
     textDoc += " @FIN@"
 
 # doc = docx.Document('Essay 3.docx')
 
 def main():
+
     running = True
     clock = pygame.time.Clock()
-
-    while running:
+    speed = 0.15
+    while True:
         clock.tick(60)
+        if running:
+            #set variables and get global variables
+            global i
+            global SCREEN_WIDTH
+            global SCREEN_HEIGHT
+            global SCALE
+            global color
 
-        #set variables and get global variables
-        global i
-        global SCREEN_WIDTH
-        global SCREEN_HEIGHT
-        global SCALE
-        global color
+            debug = False
 
-        debug = False
+            corner1 = (25, SCREEN_HEIGHT * SCALE - 75)  #Top Left corner of button 1
+            corner2 = (SCREEN_WIDTH * SCALE - 125, SCREEN_HEIGHT * SCALE - 75)  #Top Left corner of button 2
 
-        corner1 = (25, SCREEN_HEIGHT * SCALE - 75)  #Top Left corner of button 1
-        corner2 = (SCREEN_WIDTH * SCALE - 125, SCREEN_HEIGHT * SCALE - 75)  #Top Left corner of button 2
+            corner3 = (25, SCREEN_HEIGHT * SCALE - 150)  #Top Left corner of button 1
+            corner4 = (SCREEN_WIDTH * SCALE - 125, SCREEN_HEIGHT * SCALE - 150)  #Top Left corner of button 2
 
-        image_length = 100 #length of the buttons
-        image_height = 50 #height of the buttons
+            image_length = 100 #length of the buttons
+            image_height = 50 #height of the buttons
 
-        #initialize pygame and set display for the window
-        pygame.init()
-        os.environ['SDL_VIDEO_CENTERED'] = '1'
-        pygame.display.set_caption("Speed Reader")
-        screen = pygame.display.set_mode((SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE))
+            #initialize pygame and set display for the window
+            pygame.init()
+            os.environ['SDL_VIDEO_CENTERED'] = '1'
+            pygame.display.set_caption("Speed Reader")
+            screen = pygame.display.set_mode((SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE))
 
-        #draw lines on the screen
-        pygame.draw.line(screen, (255, 255, 255), (0, SCREEN_HEIGHT * SCALE / 5 - 30), (SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE / 5 - 30), 1)
-        pygame.draw.line(screen, (255, 255, 255), (0, SCREEN_HEIGHT * SCALE / 5 + 30 + 40), (SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE / 5 + 30 + 40), 1)
-        pygame.draw.line(screen, (255, 255, 255), (SCREEN_WIDTH * SCALE / 2, SCREEN_HEIGHT * SCALE / 5 - 30), (SCREEN_WIDTH * SCALE / 2, SCREEN_HEIGHT * SCALE / 5 - 20), 1)
-        pygame.draw.line(screen, (255, 255, 255), (SCREEN_WIDTH * SCALE / 2, SCREEN_HEIGHT * SCALE / 5 + 30 + 40), (SCREEN_WIDTH * SCALE / 2, SCREEN_HEIGHT * SCALE / 5 + 20 + 40), 1)
+            #draw lines on the screen
+            pygame.draw.line(screen, (255, 255, 255), (0, SCREEN_HEIGHT * SCALE / 5 - 30), (SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE / 5 - 30), 1)
+            pygame.draw.line(screen, (255, 255, 255), (0, SCREEN_HEIGHT * SCALE / 5 + 30 + 40), (SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE / 5 + 30 + 40), 1)
+            pygame.draw.line(screen, (255, 255, 255), (SCREEN_WIDTH * SCALE / 2, SCREEN_HEIGHT * SCALE / 5 - 30), (SCREEN_WIDTH * SCALE / 2, SCREEN_HEIGHT * SCALE / 5 - 20), 1)
+            pygame.draw.line(screen, (255, 255, 255), (SCREEN_WIDTH * SCALE / 2, SCREEN_HEIGHT * SCALE / 5 + 30 + 40), (SCREEN_WIDTH * SCALE / 2, SCREEN_HEIGHT * SCALE / 5 + 20 + 40), 1)
 
-        #draw the buttons
-        pygame.draw.rect(screen, (255, 255, 255), (25, SCREEN_HEIGHT * SCALE - 75, 100, 50), 1)
-        pygame.draw.rect(screen, (255, 255, 255), (SCREEN_WIDTH * SCALE - 125, SCREEN_HEIGHT * SCALE - 75, 100, 50), 1)
+            #draw the buttons
+            pygame.draw.rect(screen, (255, 0, 0), (25, SCREEN_HEIGHT * SCALE - 75, 100, 50), 0)
+            pygame.draw.rect(screen, (0, 0, 255), (SCREEN_WIDTH * SCALE - 125, SCREEN_HEIGHT * SCALE - 75, 100, 50), 0)
 
-        #initialize font
-        pygame.font.init()
-        speedFont = pygame.font.SysFont('Helvetica', 30)
-        word = ""
+            pygame.draw.rect(screen, (255, 0, 0), (25, SCREEN_HEIGHT * SCALE - 150, 100, 50), 0)
+            pygame.draw.rect(screen, (0, 255, 0), (SCREEN_WIDTH * SCALE - 125, SCREEN_HEIGHT * SCALE - 150, 100, 50), 0)
 
-        try:
-            #loop to create each word
-            while textDoc[i] != " ":
-                word = word + textDoc[i]
-                i = i + 1
-                if textDoc[i] == "\n":
+            #initialize font
+            pygame.font.init()
+            speedFont = pygame.font.SysFont('Helvetica', 30)
+            word = ""
+
+            try:
+                #loop to create each word
+                while textDoc[i] != " ":
+                    word = word + textDoc[i]
                     i = i + 1
+                    if textDoc[i] == "\n":
+                        i = i + 1
 
-        except:
-            #don't error out at the end of the file
-            word = "FIN"
+            except:
+                #don't error out at the end of the file
+                word = "FIN"
 
-            #create a textsurface
-            textsurface = speedFont.render(word, 1, (color))
+                #create a textsurface
+                textsurface = speedFont.render(word, 1, (color))
 
-            #calculate the center of the word
-            x = ((SCREEN_WIDTH / 2) * SCALE - (textsurface.get_rect().width / 2))
+                #calculate the center of the word
+                x = ((SCREEN_WIDTH / 2) * SCALE - (textsurface.get_rect().width / 2))
 
-            #display the text
-            screen.blit(textsurface, (x, SCREEN_HEIGHT * SCALE / 5))
-            pygame.display.flip()
-            sleep(5)
+                #display the text
+                screen.blit(textsurface, (x, SCREEN_HEIGHT * SCALE / 5))
+                pygame.display.flip()
 
-        #repeat until the end of the file
-        if word != "FIN":
+            #repeat until the end of the file
+            if word != "FIN":
 
-            #create a textsurface
-            textsurface = speedFont.render(word, 1, (color))
+                #create a textsurface
+                textsurface = speedFont.render(word, 1, (color))
 
-            #calculate the center of the word
-            x = ((SCREEN_WIDTH / 2) * SCALE - (textsurface.get_rect().width / 2))
+                #calculate the center of the word
+                x = ((SCREEN_WIDTH / 2) * SCALE - (textsurface.get_rect().width / 2))
 
-            #display the text
-            screen.blit(textsurface, (x, SCREEN_HEIGHT * SCALE / 5))
-            pygame.display.flip()
+                #display the text
+                screen.blit(textsurface, (x, SCREEN_HEIGHT * SCALE / 5))
+                pygame.display.flip()
 
-            #display the word in a readable manner in the console window
-            if debug:
-                print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-                print word
+                #display the word in a readable manner in the console window
+                if debug:
+                    print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+                    print word
 
-            #wait before going again
-            sleep(0.12)
-            i = i + 1
+                #wait before going agai
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                sys.exit();
-            if event.type == MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    mouse_x, mouse_y = event.pos
-                    if (mouse_x >= corner1[0]) and (mouse_x <= corner1[0]+image_length) and (mouse_y >= corner1[1]) and (mouse_y <= corner1[1]+image_height):
-                        color = (255, 0, 0)
-                        print ("Button one is selected")
-                        button1=True
-                        button2=False
-                    elif (mouse_x >= corner2[0]) and (mouse_x <= corner2[0]+image_length) and (mouse_y >= corner2[1]) and (mouse_y <= corner2[1]+image_height):
-                        color = (0, 0, 255)
-                        print ("Button two is selected")
-                        button1=False
-                        button2=True
-                    else:
-                        print ("That's not a button")
-                        button1=False
-                        button2=False
+                sleep(speed)
+                i = i + 1
 
-            #loop through program
-            main()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    sys.exit();
+                if event.type == MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        mouse_x, mouse_y = event.pos
+                        if (mouse_x >= corner1[0]) and (mouse_x <= corner1[0]+image_length) and (mouse_y >= corner1[1]) and (mouse_y <= corner1[1]+image_height):
+                            color = (255, 0, 0)
+
+                        elif (mouse_x >= corner2[0]) and (mouse_x <= corner2[0]+image_length) and (mouse_y >= corner2[1]) and (mouse_y <= corner2[1]+image_height):
+                            color = (0, 0, 255)
+
+
+                        elif (mouse_x >= corner3[0]) and (mouse_x <= corner3[0]+image_length) and (mouse_y >= corner3[1]) and (mouse_y <= corner3[1]+image_height):
+                            speed = speed + 0.02
+                            print "Speed", speed
+
+                        elif (mouse_x >= corner4[0]) and (mouse_x <= corner4[0]+image_length) and (mouse_y >= corner4[1]) and (mouse_y <= corner4[1]+image_height):
+                            speed = speed - 0.02
+                            print "Speed", speed
+
+                        else:
+                            color = (255, 255, 255)
+                            print ("That's not a button")
+                            button1=False
+                            button2=False
+
+                #loop through program
 
 #call main method
 main()
