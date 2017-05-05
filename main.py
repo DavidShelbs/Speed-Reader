f54e2a0d673e34b35acad2ca14febd046a5458ca
RyanDelap <akayungeagle@gmail.com>
2017-05-05 09:26:26 -0500

    adding light and dark modes

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import Tkinter as tk
 #set constant variables
 SCREEN_WIDTH = 144
 SCREEN_HEIGHT = 256
-SCALE = 3
+SCALE = 2
 
 color = (255, 255, 255)
 
@@ -34,7 +34,10 @@ def main():
 
     running = True
     clock = pygame.time.Clock()
-    speed = 0.15
+    speed = 0.2
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+
     while True:
         clock.tick(60)
         if running:
@@ -62,15 +65,21 @@ def main():
             pygame.display.set_caption("Speed Reader")
             screen = pygame.display.set_mode((SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE))
 
+            if color == white:
+                screen.fill(black)
+
+            if color == black:
+                screen.fill(white)
+
             #draw lines on the screen
-            pygame.draw.line(screen, (255, 255, 255), (0, SCREEN_HEIGHT * SCALE / 5 - 30), (SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE / 5 - 30), 1)
-            pygame.draw.line(screen, (255, 255, 255), (0, SCREEN_HEIGHT * SCALE / 5 + 30 + 40), (SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE / 5 + 30 + 40), 1)
-            pygame.draw.line(screen, (255, 255, 255), (SCREEN_WIDTH * SCALE / 2, SCREEN_HEIGHT * SCALE / 5 - 30), (SCREEN_WIDTH * SCALE / 2, SCREEN_HEIGHT * SCALE / 5 - 20), 1)
-            pygame.draw.line(screen, (255, 255, 255), (SCREEN_WIDTH * SCALE / 2, SCREEN_HEIGHT * SCALE / 5 + 30 + 40), (SCREEN_WIDTH * SCALE / 2, SCREEN_HEIGHT * SCALE / 5 + 20 + 40), 1)
+            pygame.draw.line(screen, (color), (0, SCREEN_HEIGHT * SCALE / 5 - 30), (SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE / 5 - 30), 1)
+            pygame.draw.line(screen, (color), (0, SCREEN_HEIGHT * SCALE / 5 + 30 + 40), (SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE / 5 + 30 + 40), 1)
+            pygame.draw.line(screen, (color), (SCREEN_WIDTH * SCALE / 2, SCREEN_HEIGHT * SCALE / 5 - 30), (SCREEN_WIDTH * SCALE / 2, SCREEN_HEIGHT * SCALE / 5 - 20), 1)
+            pygame.draw.line(screen, (color), (SCREEN_WIDTH * SCALE / 2, SCREEN_HEIGHT * SCALE / 5 + 30 + 40), (SCREEN_WIDTH * SCALE / 2, SCREEN_HEIGHT * SCALE / 5 + 20 + 40), 1)
 
             #draw the buttons
-            pygame.draw.rect(screen, (255, 0, 0), (25, SCREEN_HEIGHT * SCALE - 75, 100, 50), 0)
-            pygame.draw.rect(screen, (0, 0, 255), (SCREEN_WIDTH * SCALE - 125, SCREEN_HEIGHT * SCALE - 75, 100, 50), 0)
+            pygame.draw.rect(screen, (0, 0, 0), (25, SCREEN_HEIGHT * SCALE - 75, 100, 50), 0)
+            pygame.draw.rect(screen, (255, 255, 255), (SCREEN_WIDTH * SCALE - 125, SCREEN_HEIGHT * SCALE - 75, 100, 50), 0)
 
             pygame.draw.rect(screen, (255, 0, 0), (25, SCREEN_HEIGHT * SCALE - 150, 100, 50), 0)
             pygame.draw.rect(screen, (0, 255, 0), (SCREEN_WIDTH * SCALE - 125, SCREEN_HEIGHT * SCALE - 150, 100, 50), 0)
@@ -78,6 +87,7 @@ def main():
             #initialize font
             pygame.font.init()
             speedFont = pygame.font.SysFont('Helvetica', 30)
+            otherSpeedFont = pygame.font.SysFont('Helvetica', 18)
             word = ""
 
             try:
@@ -105,14 +115,20 @@ def main():
             #repeat until the end of the file
             if word != "FIN":
 
+                wpm = 60 / speed
+
                 #create a textsurface
                 textsurface = speedFont.render(word, 1, (color))
+                wpmsurface = otherSpeedFont.render(str(round(wpm, -1)) + " words per minute", 1, (color))
 
                 #calculate the center of the word
                 x = ((SCREEN_WIDTH / 2) * SCALE - (textsurface.get_rect().width / 2))
+                newx = ((SCREEN_WIDTH / 2) * SCALE - (wpmsurface.get_rect().width / 2))
 
                 #display the text
                 screen.blit(textsurface, (x, SCREEN_HEIGHT * SCALE / 5))
+                screen.blit(wpmsurface, (newx, SCREEN_HEIGHT * SCALE - 175))
+
                 pygame.display.flip()
 
                 #display the word in a readable manner in the console window
@@ -133,11 +149,10 @@ def main():
                     if event.button == 1:
                         mouse_x, mouse_y = event.pos
                         if (mouse_x >= corner1[0]) and (mouse_x <= corner1[0]+image_length) and (mouse_y >= corner1[1]) and (mouse_y <= corner1[1]+image_height):
-                            color = (255, 0, 0)
+                            color = white
 
                         elif (mouse_x >= corner2[0]) and (mouse_x <= corner2[0]+image_length) and (mouse_y >= corner2[1]) and (mouse_y <= corner2[1]+image_height):
-                            color = (0, 0, 255)
-
+                            color = black
 
                         elif (mouse_x >= corner3[0]) and (mouse_x <= corner3[0]+image_length) and (mouse_y >= corner3[1]) and (mouse_y <= corner3[1]+image_height):
                             speed = speed + 0.02
